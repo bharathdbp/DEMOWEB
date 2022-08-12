5fcea7bf87e3916fc36622a804df3ad442c63f69
Achyut <Achyut@DESKTOP-LB2VJ69>
2022-08-12 12:49:25 +0530

    i have created a class on your project

---
+++ DEMOWEB/src/test/java/ACHYUT/bharat.java
@@ -0,0 +1,15 @@
+package ACHYUT;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class bharat {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./Softwares/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.cadbury.com");
+
+	}
+
+}
